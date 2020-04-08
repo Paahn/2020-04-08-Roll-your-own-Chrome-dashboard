25b27e13bf09c74c1bb5033f26eb6b5e8e428938
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-04-08 13:54:20 -0700

    bulk of files

---
+++ sharks.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "images/1.jpg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "a shark");
+  document.body.appendChild(x);
+}
